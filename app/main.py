85bc7015a492bfb37b6c9272552b056ec2db6000
Ryan Firpo <ryanfirpo1@gmail.com>
2024-11-24 20:52:53 -0800

    accept socket connection

--- app/main.py
+++ app/main.py
@@ -16,7 +16,9 @@ def main():
     logger.info("Hello World!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
+
 
 
 if __name__ == "__main__":
